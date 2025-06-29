da3faa72a26637d0d7f19c55a8d8ad0640c5f050
lightningexperience <lightningexperience@users.noreply.github.com>
2025-06-28 21:40:57 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 from mcp.server.fastmcp import FastMCP
-from fastapi.responses import PlainTextResponse
 
-mcp = FastMCP("easy-mcp-server")
+mcp = FastMCP("hello-world")
 
 @mcp.tool()
 async def say_hello(name: str) -> str:
-    return f"Hello MCP Client, {name}! I am a MCP Server 🎉"
-    
+    """Say Hello to a person."""
+    return f"Hello MCP Client, {name}! I am a MCP Server!"
+
 if __name__ == "__main__":
-    mcp.run(transport="fastapi")
+    mcp.run(transport="http")  # This will expose the server on HTTP automatically
