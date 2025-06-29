3cbb034ace775dd1284aa41e0614510ab537162d
lightningexperience <lightningexperience@users.noreply.github.com>
2025-06-28 21:29:47 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,8 +4,7 @@ mcp = FastMCP("hello-world")
 
 @mcp.tool()
 async def say_hello(name: str) -> str:
-    """Say Hello to a person."""
-    return f"Hello MCP Client, {name}! I am a MCP Server!!!"
+    return f"Hello MCP Client, {name}! I am a MCP Server 🎉"
 
 if __name__ == "__main__":
-    mcp.run(transport="http")  #  serves over HTTP
+    mcp.run(transport="fastapi")
