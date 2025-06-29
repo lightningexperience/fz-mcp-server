134dde7029b392459c06392a41bab19d38620126
lightningexperience <lightningexperience@users.noreply.github.com>
2025-06-28 21:37:40 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,11 +6,6 @@ mcp = FastMCP("easy-mcp-server")
 @mcp.tool()
 async def say_hello(name: str) -> str:
     return f"Hello MCP Client, {name}! I am a MCP Server 🎉"
-
-# 👇 Add a custom root route that shows a message in the browser
-@mcp.app.get("/", response_class=PlainTextResponse)
-async def root():
-    return "MCP Server is running on Heroku!"
     
 if __name__ == "__main__":
     mcp.run(transport="fastapi")
